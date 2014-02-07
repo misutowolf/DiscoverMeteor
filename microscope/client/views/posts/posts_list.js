248da6810a0e3bf7746e13ad7a0b048eb7d5b46b
Charles Baker <assemblag3@gmail.com>
2014-02-07 10:47:32 -0500

    Section 3-2

---
+++ microscope/client/views/posts/posts_list.js
@@ -0,0 +1,26 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope'
+  },
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  },
+  {
+    title: 'Charles Baker',
+    author: 'Charles Baker',
+    url: 'http://charlesbaker.net'
+  }
+];
+
+Template.postsList.helpers({
+	posts: postsData
+});
